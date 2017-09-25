b56ffe9cea329b2a946cb0cd998fb33437c68699
Alex Whan <alexwhan@gmail.com>
2017-09-25 18:34:33 +1000

    Initial commit. Data for simulated map as well as script + data for simulated genotypes

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Genetic map and population/genotype simulation
+
+These simulations are made for generating test data for [pretzel](https://github.com/plantinformatics/pretzel) development.
+
+They depend on [R/qtl](https://github.com/kbroman/qtl). I have used a subset of markers from the 90k consensus map ordered more or less in the correct way so that comparisons can be made with real maps.
+
---
+++ scripts/sim_genos.R
@@ -0,0 +1,22 @@
+library(qtl)
+library(purrr)
+library(magrittr)
+library(tidyr)
+library(genomap)
+
+initial_map <- read.csv("data/initial_genetic_map.csv", stringsAsFactors = FALSE)
+initial_map$lg <- as.factor(initial_map$lg)
+lgs <- table(initial_map$lg)
+marker_names <- split(initial_map$marker_name, initial_map$lg)
+
+pmap <- sim.map(lgs * rnorm(21, 1, 0.1), lgs, include.x = FALSE, sex.sp = FALSE, eq.spacing = FALSE)
+names(pmap) <- names(lgs)
+
+pcross <- sim.cross(pmap, n.ind = 100)
+pcross_geno <- map2(pcross$geno, marker_names, function(.x, .y) set_colnames(.x$data, .y)) %>% 
+  map(as.data.frame) %>% 
+  map(~mutate(.x, germplasm_id = paste("sim", 1:100, sep = "_"))) %>% 
+  map(~gather(.x, marker_name, genotype, -germplasm_id)) %>% 
+  bind_rows
+
+write.csv(pcross_geno, "data/simulated_geno.csv", row.names = FALSE)

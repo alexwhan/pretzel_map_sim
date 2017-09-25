c49cdd9fc31d88de2c4b251016e089df10ef9d6d
Alex Whan <alexwhan@gmail.com>
2017-09-25 20:07:08 +1000

    changed to rils - no hets. tidied output data

--- scripts/sim_genos.R
+++ scripts/sim_genos.R
@@ -4,6 +4,7 @@ library(magrittr)
 library(tidyr)
 library(genomap)
 
+set.seed(123)
 initial_map <- read.csv("data/initial_genetic_map.csv", stringsAsFactors = FALSE)
 initial_map$lg <- as.factor(initial_map$lg)
 lgs <- table(initial_map$lg)
@@ -12,7 +13,7 @@ marker_names <- split(initial_map$marker_name, initial_map$lg)
 pmap <- sim.map(lgs * rnorm(21, 1, 0.1), lgs, include.x = FALSE, sex.sp = FALSE, eq.spacing = FALSE)
 names(pmap) <- names(lgs)
 
-pcross <- sim.cross(pmap, n.ind = 100)
+pcross <- sim.cross(pmap, n.ind = 100, type = "riself")
 pcross_geno <- map2(pcross$geno, marker_names, function(.x, .y) set_colnames(.x$data, .y)) %>% 
   map(as.data.frame) %>% 
   map(~mutate(.x, germplasm_id = paste("sim", 1:100, sep = "_"))) %>% 
